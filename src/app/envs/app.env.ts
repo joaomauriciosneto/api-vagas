661975ae26832a6afdb6b9a0dc4415841ff7cc81
pviniciusm <pvinics@gmail.com>
2022-11-29 20:46:05 -0300

    add database schema to env

--- src/app/envs/app.env.ts
+++ src/app/envs/app.env.ts
@@ -3,5 +3,5 @@ import "dotenv/config";
 export const appEnv = {
     port: process.env.PORT,
     databaseUrl: process.env.DB_URL,
-    databaseSchema: process.env.SCHEMA,
+    databaseSchema: process.env.DB_SCHEMA,
 };
