48605f7724f712f4c00f6a0afcd3e1899ec77a52
Lucas Silveira <lucasroennausilveira@gmail.com>
2022-11-30 21:00:50 -0300

    criação da rota candidato

--- src/main/server/express.routes.ts
+++ src/main/server/express.routes.ts
@@ -1,15 +1,17 @@
 import { Router, Express } from "express";
+import { candidatoRoutes } from "../../app/features/candidato/routes/candidato.routes";
 import { loginRoutes } from "../../app/features/login/routes/login.routes";
 import { recrutadorRoutes } from "../../app/features/recrutador/routes/recrutador.routes";
 
 export const createRoutes = (app: Express) => {
-    app.get("/", (req, res) => {
-        return res.status(200).send({
-            ok: true,
-            message: "Tudo ok!",
-        });
+  app.get("/", (req, res) => {
+    return res.status(200).send({
+      ok: true,
+      message: "Tudo ok!",
     });
+  });
 
-    app.use("/recrutador", recrutadorRoutes());
-    app.use("/auth", loginRoutes());
+  app.use("/recrutador", recrutadorRoutes());
+  app.use("/auth", loginRoutes());
+  app.use("/candidato", candidatoRoutes());
 };
